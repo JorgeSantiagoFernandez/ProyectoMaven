69413e20a9f8bf0772b81601671e8deace6ce7c1
Teroke <96469779+Teroke@users.noreply.github.com>
2021-12-21 10:21:29 +0100

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,56 @@
+package com.ecodeup.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usuario")
+
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idUser;
+	@Column
+	private String user;
+	@Column
+	private String password;
+	@Column
+	private int rol;
+	
+	public Long getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(Long idUser) {
+		this.idUser = idUser;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getRol() {
+		return rol;
+	}
+	public void setRol(int rol) {
+		this.rol = rol;
+	}
+	@Override
+	public String toString() {
+		return "Login [idUser=" + idUser + ", user=" + user + ", password=" + password + ", rol=" + rol + "]";
+	}
+	
+
+
+}
+
